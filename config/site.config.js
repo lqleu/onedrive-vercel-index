63260616a1578ce24b62240263ae762f99c2e58f
lqleu <122720737+lqleu@users.noreply.github.com>
2023-06-21 22:11:13 +0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Хмара",
+  title: "UD",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
@@ -53,7 +53,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/lqleu/onedrive-vercel-index',
+      link: 'https://github.com/lqleu/ud',
     },
   ],
 
